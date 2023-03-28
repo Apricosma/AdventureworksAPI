faac1b895610d85180e06ee092abc9dc199f75c1
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-03-28 11:32:38 -0500

    Add model scaffolding and connection strings

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using AdventureworksAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLt2019Context")));
+
 var app = builder.Build();
 
 app.Run();
