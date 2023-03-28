516643ec2f6986d6c7ed08ff582d47893f33de64
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-03-28 14:17:15 -0500

    Add read and create methods for Address

--- Methods/AddressMethods.cs
+++ Methods/AddressMethods.cs
@@ -1,6 +1,36 @@
-﻿namespace AdventureworksAPI.Methods
+﻿using AdventureworksAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureworksAPI.Methods
 {
     public class AddressMethods
     {
+        public static IResult GetAddresses(AdventureWorksLt2019Context db)
+        {
+            return Results.Ok(db.Addresses);
+        }
+
+        public static IResult GetAddressById(AdventureWorksLt2019Context db, int id)
+        {
+            Address address = db.Addresses.FirstOrDefault(a => 
+                a.AddressId == id);
+
+            if (address == null)
+            {
+                return Results.NotFound(id);
+            }
+
+            return Results.Ok(address);
+        }
+
+        public static IResult CreateAddress(AdventureWorksLt2019Context db
+            , Address address)
+        {
+            db.Addresses.Add(address);
+            db.SaveChanges();
+
+            return Results.Created($"/address/create", address);
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using AdventureworksAPI.Methods;
 using AdventureworksAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,5 +9,10 @@ builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLt2019Context")));
 
 var app = builder.Build();
+
+app.MapGet("/address", AddressMethods.GetAddresses);
+app.MapGet("/address/{id:int}", AddressMethods.GetAddressById);
+app.MapPost("/address/create", AddressMethods.CreateAddress);
+
 
 app.Run();
