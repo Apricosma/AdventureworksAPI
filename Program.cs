53982badd79db1c4a4babe6c8f123eda512eadc2
chamanbawa <chamanbawa12@gmail.com>
2023-03-29 09:26:40 -0500

    Customer_Resource_Basic_Endpoints done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using AdventureworksAPI.Methods;
 using AdventureworksAPI.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -8,5 +11,12 @@ builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLt2019Context")));
 
 var app = builder.Build();
+
+
+app.MapPost("/Customer/create",CustomerMethods.CreateCustomer );
+app.MapGet("/Customer/GetCustomerWithId", CustomerMethods.GetCustomerwithId);
+app.MapGet("/Customer", CustomerMethods.GetAllCustomers);
+app.MapDelete("/Customer/Delete", CustomerMethods.DeleteCustomer);
+app.MapPut("Customer/Update", CustomerMethods.UpdateCustomer);
 
 app.Run();
